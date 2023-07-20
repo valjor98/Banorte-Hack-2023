1d906e305e3a282888f9b14418cbaa3fd19c430c
valjor98 <valjor98@gmail.com>
2023-07-19 19:30:19 -0600

    Deleted unecessary code blocks in index.js

--- functions/index.js
+++ functions/index.js
@@ -5,31 +5,13 @@ const serviceAccount = require("./service-account.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://srbanorte23-default-rtdb.firebaseio.com/",
+  databaseURL: "https://srbanorte23-default-rtdb.firebaseio.com",
 });
 
 const {SessionsClient} = require("dialogflow");
-const {WebhookClient} = require("dialogflow-fulfillment");
+const { WebhookClient } = require('dialogflow-fulfillment');
 
 const db = admin.firestore();
-
-// Create the users collection if it doesn't exist
-const usersCollection = db.collection("users");
-usersCollection
-  .get()
-  .then((snapshot) => {
-    if (snapshot.empty) {
-      console.log("Creating users collection...");
-      return usersCollection.doc("dummy").set({});
-    }
-    return Promise.resolve();
-  })
-  .then(() => {
-    console.log("Users collection exists or has been created successfully");
-  })
-  .catch((error) => {
-    console.error("Error creating users collection:", error);
-  });
 
 
 exports.dialogflowGateway = functions.https.onRequest((request, response) => {
@@ -49,63 +31,202 @@ exports.dialogflowGateway = functions.https.onRequest((request, response) => {
   });
 });
 
-exports.dialogflowWebhook = functions.https.onRequest(async (request, response) => {
-  const agent = new WebhookClient({request, response});
 
-  async function makeTransactionsHandler(agent) {
-    const {amount} = agent.parameters;
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+ 
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
-    // Obtener la referencia del documento del usuario
-    const userRef = db.collection("users").doc("jeffd23");
+let age = 35;
+let category = "adult";
+let currentBalance = 6450;
+let email = "alexm01@gmail.com";
+let income = 5000;
+let expenses = 3400;
+let first_name = "Daniel";
+let last_name = "Loredo";
+let phone_number = 8112341234;
+let pronouns = "el";
+let trans_amount = [100];
+let trans_subject = ["pizza"];
+let trans_date = ["10-10-23"];
 
-    try {
-      // Obtener el documento del usuario
-      const userDoc = await userRef.get();
+let user_emails = ["soraya.arriaga.maldonado@banorte.com"];
+let user_links = ["banorte.com"];
+let user_messages = ["texto"];
+let user_phones = [8165381364];
 
-      if (userDoc.exists) {
-        const userData = userDoc.data();
-        const currentBalance = userData.balance;
 
-        if (currentBalance >= amount) {
-          const updatedBalance = currentBalance - amount;
 
-          // Actualizar el saldo del usuario
-          await userRef.update({balance: updatedBalance});
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
-          agent.add(`Transaction successful. Your updated balance is $${updatedBalance}.`);
-        } else {
-          agent.add(`Insufficient balance. Your current balance is $${currentBalance}.`);
-        }
-      } else {
-        agent.add("User data not found");
-      }
-    } catch (error) {
-      console.error("Error getting user document:", error);
-      agent.add(`An error occurred. Please try again later.`);
-    }
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
   }
 
-  async function universityHandler(agent) {
-    // Perform financial coaching or answer finance-related questions based on the user's query
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
   }
 
-  async function fraudDetectionHandler(agent) {
-    const {email} = agent.parameters;
-
-    // Verificar si el correo electrónico está en la colección de fraud_emails
-    const fraudListRef = db.collection("fraud_emails");
-    const fraudListSnapshot = await fraudListRef.where("email", "==", email).get();
-
-    if (fraudListSnapshot.empty) {
-      agent.add(`This email is not recognized as a known scam email.`);
+  // FINANZAS
+  // Obtener saldo
+  function obtenerSaldoActualHandler(agent) {
+    let message = '';
+    if (currentBalance > 10000) {
+      message = `Tu saldo actual es de $${currentBalance}. ¡Excelente trabajo en mantener un saldo saludable en tu cuenta!`;
+    } else if (currentBalance >= 5000) {
+      message = `Tu saldo actual es de $${currentBalance}. Recuerda seguir ahorrando y controlando tus gastos para mantener un buen equilibrio financiero.`;
     } else {
-      agent.add(`Warning: This email is associated with known scams.`);
+      message = `Tu saldo actual es de $${currentBalance}. Te recomendaría revisar tus gastos y encontrar formas de ahorrar para fortalecer tu situación financiera.`;
+    }
+    agent.add(message);
+  }
+
+  // Intent para calcular ahorro mensual
+  function calcularAhorroMensualHandler(agent) {
+    let message = '';
+    if (income > 10000) {
+      const savingsPercentage = 20; // Example: Saving 20% of income
+      const savingsAmount = (income * savingsPercentage) / 100;
+      message = `Según tus ingresos actuales, te sugiero ahorrar al menos el ${savingsPercentage}% de tus ingresos mensuales, lo que equivale a $${savingsAmount}. Esto te permitirá construir un fondo de emergencia y trabajar hacia tus metas financieras.`;
+    } else if (income >= 5000) {
+      const savingsPercentage = 10; // Example: Saving 10% of income
+      const savingsAmount = (income * savingsPercentage) / 100;
+      message = `Considerando tus ingresos actuales, sería recomendable destinar al menos el ${savingsPercentage}% de tus ingresos mensuales al ahorro, lo que representa aproximadamente $${savingsAmount}. Esto te ayudará a crear una base financiera sólida.`;
+    } else {
+      const savingsAmount = 100; // Example: Saving $100 per month
+      message = `Dado tus ingresos actuales, te recomendaría ahorrar lo máximo posible, aunque sea un monto pequeño, como $${savingsAmount} al mes. Cada ahorro cuenta y te ayudará a establecer buenos hábitos financieros.`;
+    }
+    agent.add(message);
+  }
+
+  // Intent para planificar presupuesto mensual
+  function planificarPresupuestoMensualHandler(agent) {
+    let message = '';
+    if (expenses < income) {
+      message = `Para planificar un presupuesto mensual, te sugiero asignar una parte de tus ingresos a diferentes categorías de gastos, como vivienda, transporte, alimentación y entretenimiento. Recuerda mantener un equilibrio entre tus gastos y ahorros para alcanzar tus metas financieras.`;
+    } else if (expenses > income) {
+      message = `Es importante priorizar tus gastos y encontrar formas de reducirlos para que se ajusten a tus ingresos. Te puedo ayudar a identificar áreas en las que podrías ahorrar, como buscar opciones más económicas para tus necesidades básicas y eliminar gastos innecesarios.`;
+    } else {
+      message = `¡Excelente! Parece que has logrado mantener un equilibrio en tus gastos. Recuerda seguir monitoreando tus gastos y ajustar tu presupuesto según sea necesario.`;
+    }
+    agent.add(message);
+  }
+
+
+
+  // FRAUDES
+  // Intent para verificar enlace web
+  function verificarEnlaceWebHandler(agent) {
+    const userLink = agent.parameters.user_link;
+    
+    if (user_links.includes(userLink)) {
+      agent.add(`Sí, el enlace ${userLink} es seguro y no representa un riesgo de fraude.`);
+    } else {
+      agent.add(`No, el enlace ${userLink} no es seguro. Podría llevar a una página de phishing o contener malware.`);
     }
   }
+
+  // Intent para verificar número de teléfono
+  function verificarNumeroTelefonoHandler(agent) {
+    const userPhone = agent.parameters.user_phone;
+    
+    if (user_phones.includes(userPhone)) {
+      agent.add(`Sí, el número de teléfono ${userPhone} es legítimo y seguro.`);
+    } else {
+      agent.add(`No, el número de teléfono ${userPhone} no es válido. Podría ser parte de un intento de fraude.`);
+      agent.add(`Hemos detectado que el número de teléfono ${userPhone} no es seguro. Te recomendamos no proporcionar información personal.`);
+    }
+  }
+
+  // Intent para verificar correo electrónico
+  function verificarCorreoElectronicoHandler(agent) {
+    const userEmail = agent.parameters.user_email;
+    
+    if (user_emails.includes(userEmail)) {
+      agent.add(`Sí, el correo electrónico ${userEmail} es válido y seguro.`);
+    } else {
+      agent.add(`No, el correo electrónico ${userEmail} no es válido. Podría ser parte de un intento de fraude.`);
+      agent.add(`Hemos detectado que el correo electrónico ${userEmail} no es seguro. Te recomendamos no interactuar con él.`);
+    }
+  }
+
+  //TRANSACCIONES
+  // Intención: Realizar transferencia
+  function realizarTransferenciaHandler(agent) {
+    //const amount = agent.parameters.amount;
+    //const accountNumber = agent.parameters.account_number;
+    const amount = 100;
+    const accountNumber = 910282;
+
+    currentBalance = currentBalance - amount;
+
+    agent.add(`Claro, podemos ayudarte a realizar la transferencia de $${amount} a la cuenta ${accountNumber}`);
+    agent.add(`La transaccion ha sido exitosa, ahora su saldo es de $${currentBalance - amount}`);
+  }
+
+  // Intención: Verificar transacción reciente
+  function verificarTransaccionRecienteHandler(agent) {
+    const amountt = agent.parameters.amount;
+    const index = trans_amount.indexOf(amountt);
+
+    if (index !== -1) {
+      const transactionSubject = trans_subject[index];
+      const transactionDate = trans_date[index];
+
+      agent.add(`La transacción de $${amountt} corresponde a ${transactionSubject} realizada el ${transactionDate}.`);
+      agent.add(`Hemos identificado la transacción de $${amountt} en tu cuenta, realizada el ${transactionDate}. Está relacionada con ${transactionSubject}.`);
+      agent.add(`La transacción de $${amountt} fue registrada en tu cuenta el ${transactionDate}. Está asociada a ${transactionSubject}.`);
+    } else {
+      agent.add(`No se encontró ninguna transacción reciente de $${amountt} en tu cuenta.`);
+    }
+  }
+
+
+  //EDGE CASES
+  // Intención: Actualizar pronombres
+  function actualizarPronombresHandler(agent) {
+    const pronombresAnteriores = pronouns;
+    const pronouns = pronombresAnteriores;
+    const nuevosPronombres = agent.parameters.nuevos_pronombres;
+
+    let message = `¡Por supuesto! A partir de ahora, me referiré a ti como "${nuevosPronombres}" en lugar de "${pronombresAnteriores}". Me alegra ver que estás expresando tu identidad y tus pronombres de una manera auténtica. Como siempre, estaré aquí para ayudarte en lo que necesites, ${nuevosPronombres}. ¡Si tienes alguna otra pregunta o solicitud, no dudes en decírmelo!`;
+
+    agent.add(message);
+  }
+
+// Intent: Automatizar pagos de estacionamiento
+function automatizarPagosEstacionamientoHandler(agent) {
+  const firstName = first_name;
+
+  const montoMaximoDiario = agent.parameters.pesos;
+  const duracionAutomatizacion = agent.parameters.dias;
+
+  agent.add(`¡Perfecto, ${firstName}! He configurado el servicio de pago automático para tus pagos de estacionamiento en la Torre Koi. A partir de ahora, los pagos se realizarán de manera automática, dentro de los límites establecidos.`);
+  agent.add(`Recuerda que siempre puedes modificar o cancelar la automatización en cualquier momento. Si tienes alguna pregunta o necesitas ayuda adicional, no dudes en preguntar.`);
+}
+
 
   let intentMap = new Map();
-  intentMap.set("MakeTransactions", makeTransactionsHandler);
-  intentMap.set("University", universityHandler);
-  intentMap.set("FraudDetection", fraudDetectionHandler);
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  //Finanzas
+  intentMap.set('Finance_obtener_saldo', obtenerSaldoActualHandler);
+  intentMap.set('Finance_calcular_ahorro_mensual', calcularAhorroMensualHandler);
+  intentMap.set('Finance_planificar_presupuesto_mensual', planificarPresupuestoMensualHandler);
+  //Fraudes
+  intentMap.set('Fraude_verificarEnlaceWeb', verificarEnlaceWebHandler);
+  intentMap.set('Fraude_verificarNumeroTelefono', verificarNumeroTelefonoHandler);
+  intentMap.set('Fraude_verificarCorreoElectronico', verificarCorreoElectronicoHandler);
+  //Transacciones
+  intentMap.set('Trans_realizarTransferencia', realizarTransferenciaHandler);
+  intentMap.set('Trans_verificarTransaccionReciente', verificarTransaccionRecienteHandler);
   agent.handleRequest(intentMap);
+  //Edge Cases
+  intentMap.set('EDGE_pronombres', actualizarPronombresHandler);
+  intentMap.set('EDGE_automatizarPagos_2', automatizarPagosEstacionamientoHandler);
 });
